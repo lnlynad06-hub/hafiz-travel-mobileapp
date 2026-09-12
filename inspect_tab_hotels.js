342eefbe88a25aa6cb2369cc2985e8f4ef4a5c1d
DanialSinclaire <danialhalim6969@gmail.com>
2026-09-12 17:24:26 +0800

    chore: clean up inspect_tab_hotels.js

--- inspect_tab_hotels.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const basePath = 'C:/Users/dania/Downloads/HAFIZ TRAVEL & TOUR';
-const blade = fs.readFileSync(basePath + '/resources/views/company/packages/index.blade.php', 'utf8');
-
-const start = blade.indexOf('id="tab-hotels"');
-const end = blade.indexOf('id="tab-pricing"');
-console.log(blade.substring(start, end));
